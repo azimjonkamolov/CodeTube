9527db3ab2d2bfee9babe06b04bcbfef540c96fb
mynameisone <azimjonkamolovuz@gmail.com>
2020-04-02 00:06:19 +0900

    navbar

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CodeTube</title>
+
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+</head>
+<body>
+    
+    <div id="pageContainer">
+        
+        <div id="mastHeadContainer">
+            hello
+        </div>
+
+        <div id="sideNavContainer">
+        
+        </div>
+
+        <div id="mainSectionContainer">
+        
+        </div>
+
+    </div>
+</body>
+</html>
