0714a7c9856bd1ea4c5d03b41e22f53bfd684dca
Azimjon Kamolov <azimjonkamolovuz@gmail.com>
2020-04-02 04:10:49 +0900

    up to seachr bar

--- index.php
+++ index.php
@@ -3,7 +3,14 @@
 <head>
     <title>CodeTube</title>
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
 
 </head>
 <body>
@@ -11,7 +18,13 @@
     <div id="pageContainer">
         
         <div id="mastHeadContainer">
-            hello
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/codev.png" title="logo" alt="Site logo">
+            </a>
         </div>
 
         <div id="sideNavContainer">
@@ -19,6 +32,10 @@
         </div>
 
         <div id="mainSectionContainer">
+
+            <div id="mainContentContainer">
+                Hello
+            </div>
         
         </div>
 
