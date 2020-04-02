be90cee7b754df6569024c2efad88fba6b824c1d
Azimjon Kamolov <azimjonkamolovuz@gmail.com>
2020-04-02 12:08:21 +0900

    top nav done

---
+++ includes/header.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>CodeTube</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
+    
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="assets/js/commonActions.js"></script>
+
+</head>
+<body>
+    
+    <div id="pageContainer">
+        
+        <div id="mastHeadContainer">
+            <button class="navShowHide">
+                <img src="assets/images/icons/menu.png">
+            </button>
+
+            <a class="logoContainer" href="index.php">
+                <img src="assets/images/icons/codev.png" title="logo" alt="Site logo">
+            </a>
+
+            <div class="searchBarContainer">
+                <form action="search.php" method="GET">
+                    <input type="text" class="searchBar" name="term" placeholder="Search">
+                    <button class="searchButton">
+                        <img src="assets/images/icons/search.png">
+                    </button>
+                </form>
+            </div>
+
+            <div class="rightIcons">
+                <a href="upload.php">
+                    <img class="upload" src="assets/images/icons/upload.png">
+                </a>
+
+                <a href="#">
+                    <img class="upload" src="assets/images/profilePictures/default.png">
+                </a>
+            </div>
+
+        </div>
+
+        <div id="sideNavContainer">
+        
+        </div>
+
+        <div id="mainSectionContainer">
+
+            <div id="mainContentContainer">
--- index.php
+++ index.php
@@ -1,44 +1,7 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>CodeTube</title>
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/style.css">
-
-    
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <script src="assets/js/commonActions.js"></script>
-
-</head>
-<body>
-    
-    <div id="pageContainer">
-        
-        <div id="mastHeadContainer">
-            <button class="navShowHide">
-                <img src="assets/images/icons/menu.png">
-            </button>
-
-            <a class="logoContainer" href="index.php">
-                <img src="assets/images/icons/codev.png" title="logo" alt="Site logo">
-            </a>
-        </div>
-
-        <div id="sideNavContainer">
-        
-        </div>
-
-        <div id="mainSectionContainer">
-
-            <div id="mainContentContainer">
+<?php
+    require_once("includes/header.php");
+?>
                 Hello
-            </div>
-        
-        </div>
-
-    </div>
-</body>
-</html>
+<?php
+    require_once("includes/footer.php");
+?>
